4018f95b71a8712acc0b710639117fa4a301af37
C-Chafik <qatar75020@gmail.com>
2023-10-03 03:01:26 +0200

    [+] P1 is finished but need to add package to have ifconfig

--- p1/k3s_server.sh
+++ p1/k3s_server.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 echo "**** Begin installing k3s server"
+
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo apt-get install -y curl
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-iface=eth1" K3S_KUBECONFIG_MODE="644" sh -
+# Saving the token for the agent in the shared
+cp /var/lib/rancher/k3s/server/node-token /vagrant_shared/k3s_server_token
+
 echo "**** End installing k3s server"
 
-echo "**** Begin installing kubectl"
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
 echo "alias k='kubectl'" >> .bashrc
-echo "**** End installing kubectl"
-
--- p1/k3s_worker.sh
+++ p1/k3s_worker.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 echo "**** Begin installing k3s worker"
+
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-iface=eth1" K3S_KUBECONFIG_MODE="644" sh -
+sudo apt-get install -y curl
+
+TOKEN=$(cat /vagrant_shared/k3s_server_token)
+curl -sfL https://get.k3s.io/ | INSTALL_K3S_EXEC="--flannel-iface=eth1" K3S_KUBECONFIG_MODE="644" K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=${TOKEN} sh -
+
 echo "**** End installing k3s worker"
+
+# Clear the token
+rm -rf /vagrant_shared/k3s_server_token
+
+echo "alias k='kubectl'" >> .bashrc
